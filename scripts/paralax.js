823ea84ca64ae506ab3201ceab4c9abaa15a30f1
Evgeniy Sannikov <e.d.sannikof@gmail.com>
2022-01-10 22:42:03 +0300

    Правки по главной стрнице

--- scripts/paralax.js
+++ scripts/paralax.js
@@ -1,12 +1,12 @@
 const guy = document.querySelector('.paralax__guy');
-const snow = document.querySelector('.paralax__snow');
+// const snow = document.querySelector('.paralax__snow');
 
 window.addEventListener('scroll', function() {
     let value = window.pageYOffset / 10
     if (value < 50) {
         guy.style.transition = 'all 5s'   
         guy.style.transform = 'translate(' + value * 7 + 'px, ' + value * 2 + 'px)';
-        snow.style.transition = 'all 5s'   
-        snow.style.transform = 'translate(' + value * 7 + 'px, ' + value * 2 + 'px)';
+        // snow.style.transition = 'all 5s'   
+        // snow.style.transform = 'translate(' + value * 7 + 'px, ' + value * 2 + 'px)';
     }
 });
